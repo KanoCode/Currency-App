9ce353145270ac31c9da83d2e93905bf86840a89
KanoCode <kanokanombola@gmail.com>
2022-06-25 12:37:55 +0200

    Update apikey, add redux toolkit

--- src/components/Utils/API.js
+++ src/components/Utils/API.js
@@ -1,7 +1,7 @@
 /* eslint-disable import/prefer-default-export */
 export const ApiKey = 'ac8970e975713382db0ae18e52198fa1'; // this is a borrowed key'bbce26616140cb78fbac5081d8a07a35'
 export const API = 'https://financialmodelingprep.com/api/v3/';
-export const ApiKey1 = 'bb6ebeedf4a3ca892598319f7aa01459';
-export const ApiKey2 = 'dd698abacd428895aa9c57011e8928a4';
-export const ApiKey3 = '1163b59677a27f2f95d06597f7fb6b44';
+export const ApiKey1 = 'c6f4da6c19a3ecb161aafc2cf9104952';
+export const ApiKey2 = 'd4e1991d47f8cfa0f5fcf3655cb78d09';
+export const ApiKey3 = '7280e354c9fe19801e27992609e67d30';
 export const imgUrlGenerator = (code) => `https://financialmodelingprep.com/image-stock/${code}.png`;
